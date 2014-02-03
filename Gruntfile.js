4f0caab21de7dd219709db4b11e08c5c2540d71d
Qi Junyuan <conan007ai@gmail.com>
2014-02-03 15:21:52 +0800

    ability to loop lanuage files and generate target files

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+/*
+ * tb.grunt-i18n
+ * https://github.com/teambition/grunt-i18n
+ *
+ * Copyright (c) 2014 Qi Junyuan
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp', 'test/tmp'],
+    },
+
+    // Configuration to be run (and then tested).
+    tb_grunt_i18n: {
+      default_options: {
+        expand: true,
+        cwd: 'test/raw',
+        src: ['**/*'],
+        dest: 'test/tmp/',
+        localesPath: 'test/raw/locales'
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'tb_grunt_i18n', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/tb_grunt_i18n.js
@@ -0,0 +1,80 @@
+/*
+ * tb.grunt-i18n
+ * https://github.com/teambition/grunt-i18n
+ *
+ * Copyright (c) 2014 Qi Junyuan
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('tb_grunt_i18n', 'An i18n builder to be used under grunt.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      localesPath: '',
+      defaultLanguage: 'en'
+    });
+
+    var localesPath = this.data.localesPath;
+    var localeFiles = [];
+    var dest = this.data.dest;
+    var cwd = this.data.cwd;
+    var defaultLanguage = options.defaultLanguage;
+
+    if (!localesPath || !grunt.file.isDir(localesPath)) {
+      grunt.log.warn('Locales path is not valid');
+    }
+
+    grunt.file.recurse(localesPath, function(abspath, rootdir, subdir, filename){
+      var localeName = filename.split('.')[0];
+      if (grunt.file.exists(abspath)) {
+        var localeFile = {
+          name: localeName,
+          file: grunt.file.readJSON(abspath)
+        };
+        localeFiles.push(localeFile);
+        grunt.file.mkdir(dest + 'i18n');
+      }
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      var files = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (grunt.file.isDir(filepath)) {
+          return false;
+        }
+
+        if (grunt.file.isMatch(localesPath + '**/*', filepath)) {
+          return false;
+        }
+
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      });
+
+      if (files.length > 0) {
+        files.map(function(filepath) {
+          for (var i = 0; i < localeFiles.length; i++) {
+            var localeName = localeFiles[i].name;
+            var targetFilepath = filepath.replace(cwd, dest + 'i18n/' + localeName);
+            if (localeName === defaultLanguage) {
+              grunt.file.write(f.dest, grunt.file.read(filepath));
+              grunt.log.writeln('File "' + f.dest + '" created.');
+            }
+            grunt.file.write(targetFilepath, grunt.file.read(filepath));
+            grunt.log.writeln('File "' + targetFilepath + '" created.');
+          }
+        });
+      }
+
+    });
+  });
+
+};
